d1a2439f29b5cb5abd9bdb6326d73616994160f8
Jhow Rodrigues <jhonatta_rodrigues@hotmail.com>
2023-05-02 08:31:30 -0300

    atualization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,40 @@
-import React, { useContext } from 'react'
-import { makeStyles } from '@mui/styles';
-import { AppBar,TextField,Toolbar, IconButton, Button} from '@mui/material';
-import MenuIcon from '@material-ui/icons/Menu'
-import { AccountCircle,VideoCall,Apps, MoreVert } from '@material-ui/icons';
-import LogoPreto from '../assets/logopreto.png'
-import { GlobalContext } from '../context';
+import React, { useContext } from "react";
+import { makeStyles } from "@mui/styles";
+import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
+import MenuIcon from "@material-ui/icons/Menu";
+import { AccountCircle, VideoCall, Apps, MoreVert } from "@material-ui/icons";
+import LogoPreto from "../assets/logopreto.png";
+import { GlobalContext } from "../context";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
 
-const useStyles = makeStyles((theme)=>({
-    root:{
-     height:'100vh',
-   }, 
-   icons:{
-     paddingRight: theme.spacing(5),
-   },menuIcon:{
-     paddingRight:theme.spacing(5),
-     paddingLeft:theme.spacing(4)
-   },
-   grow:{
-     flexGrow:1
-   },logo:{
-     height:25,
-     cursor:'pointer'
-   }
- }));
- 
+  },
+  icons: {
+    paddingRight: theme.spacing(5),
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(4),
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  logo: {
+    height: 25,
+    cursor: "pointer",
+  },
+}));
+
 const Header = () => {
-const {handleDrawerToggle} = useContext(GlobalContext)
-    const classes = useStyles()
+  const { handleDrawerToggle } = useContext(GlobalContext);
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar color='inherit' elevation={0} sx={{zIndex:1201}}>
-        <Toolbar>
+      <AppBar color="inherit" elevation={0} sx={{ zIndex: 1201, display:'flex'}}>
+        <Toolbar style={{ display: 'flex', justifyContent: 'space-between'}}>
           <IconButton
             size="large"
             edge="start"
@@ -41,36 +43,31 @@ const {handleDrawerToggle} = useContext(GlobalContext)
             sx={{ mr: 2 }}
             onClick={handleDrawerToggle}
           >
-           <MenuIcon />
+            <MenuIcon />
           </IconButton>
-          <img src={LogoPreto} alt="logo do YouTube"  className={classes.logo}/>
-          <div className={classes.grow}/>
-          <IconButton
-            color="inherit"
-            className={classes.menuIcon}
+       
+          <img src={LogoPreto} alt="logo do YouTube" className={classes.logo} />
+          <div className={classes.grow} />
+          <IconButton color="inherit" className={classes.menuIcon}>
+            <VideoCall />
+          </IconButton>
+          <IconButton color="inherit" className={classes.icons}>
+            <Apps />
+          </IconButton>
+          <IconButton color="inherit" className={classes.icons}>
+            <MoreVert />
+          </IconButton>
+          <Button
+            variant="outlined"
+            color="secondary"
+            startIcon={<AccountCircle />}
           >
-           <VideoCall />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            className={classes.icons}
-          >
-           <Apps />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            className={classes.icons}
-          >
-                 
-           <MoreVert />
-          </IconButton>
-      
-          <Button variant='outlined' color="secondary" startIcon={<AccountCircle/>}>Fazer Login</Button>
+            Fazer Login
+          </Button>
         </Toolbar>
       </AppBar>
-  
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
